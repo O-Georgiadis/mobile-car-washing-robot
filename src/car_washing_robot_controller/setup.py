d949a05e6892368a638cf74c3ee5a937c45d126c
johnpapwinter <johnpapahimonas@yahoo.com>
2026-01-02 21:24:36 +0200

    minor

--- src/car_washing_robot_controller/setup.py
+++ src/car_washing_robot_controller/setup.py
@@ -28,8 +28,6 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'simple_controller = car_washing_robot_controller.simple_controller:main',
-            'lidar_test_node = car_washing_robot_controller.lidar_test_node:main',
             'approach_node = car_washing_robot_controller.nodes.approach_node:main',
         ],
     },
